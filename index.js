ea9324e1f9c32de3801c94695e2600a55cdddc07
Mei Li <mei.qli3@gmail.com>
2018-05-14 19:51:10 -0400

    all things

---
+++ index.js
@@ -0,0 +1,32 @@
+const path = require('path')
+const express = require('express')
+const exphbs = require('express-handlebars')
+
+const app = express()
+const port = 3000
+
+app.engine('.hbs', exphbs({
+  defaultLayout: 'main',
+  extname: '.hbs',
+  layoutsDir: path.join(__dirname, 'views/layouts')
+}))
+app.set('view engine', '.hbs')
+app.set('views', path.join(__dirname, 'views'))
+
+app.get('/', (request, response) => {
+  response.render('home')
+})
+
+app.use((err, request, response, next) => {
+  // log the error, for now just console.log
+  console.log(err)
+  response.status(500).send('Something broke!')
+})
+
+app.listen(port, (err) => {
+  if (err) {
+    return console.log('something bad happened', err)
+  }
+
+  console.log(`server is listening on ${port}`)
+})
