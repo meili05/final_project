4af992de644de087c1665586dfc0fc7c87d0d374
Mei Li <mei.qli3@gmail.com>
2018-05-16 20:29:26 -0400

    fixing port

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const express = require('express')
 const exphbs = require('express-handlebars')
 
 const app = express()
-const port = 3000
+const port = process.env.PORT || 3000;
 
 app.engine('.hbs', exphbs({
   defaultLayout: 'main',
